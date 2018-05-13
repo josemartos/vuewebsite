98d2e68ff299e6673ece3215bb95dbf0967af2c1
Jose <martosjosele@gmail.com>
2018-05-13 13:09:41 +0200

    Simpler css on components since it's scoped

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,9 +1,9 @@
 <template>
-    <header class="header">
-        <a href="/" title="martos website" class="header_logo">martos website</a>
+    <header>
+        <a href="/" title="martos website" class="logo">martos website</a>
 
-        <h1 class="header_title">Jos&eacute; Manuel Martos Romero</h1>
-        <p class="header_position">senior front-end developer at <a href="https://www.pantaflix.com/" title="Pantaflix" target="_blank">Pantaflix</a></p>
+        <h1 class="title">Jos&eacute; Manuel Martos Romero</h1>
+        <p class="position">senior front-end developer at <a href="https://www.pantaflix.com/" title="Pantaflix" target="_blank">Pantaflix</a></p>
     </header>
 </template>
 
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,8 +1,8 @@
 <template>
     <article class="home">
-        <nav class="home_nav">
+        <nav class="nav">
             <div class="page-container">
-                <ul class="home_navList">
+                <ul class="nav_list">
                     <li><a href="https://www.flickr.com/photos/martosjose/" target="_blank" title="flickr profile">photography</a></li>
                     <li><a title="resume" href="/assets/pdf/CV_JoseMartos.pdf" download>resume</a></li>
                     <li><a href="http://www.eldivandesigmund.com/" target="_blank" title="El diván de Sigmund">writings</a></li>
@@ -10,7 +10,7 @@
             </div>
         </nav>
 
-        <div class="home_greetings">
+        <div class="greetings">
             <a href="mailto:martosjosele@gmail.com?subject=Hello%20Jos%C3%A9%20M.%20Martos" title="Hi Jose">s@y hello</a>
         </div>
     </article>
